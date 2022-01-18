df7fdc165103ffddf5e17dc7c3e0aea0f649aa38
kimseongkyeong <isd3554@naver.com>
2022-01-18 23:04:22 +0900

    Add Node.js Express ServerModule

---
+++ client/src/App.js
@@ -0,0 +1,88 @@
+//App.js
+import "./App.css";
+import React from "react";
+import Customer from "./components/Customer";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "박건형",
+    birthday: "980216",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "강하늘",
+    birthday: "980414",
+    gender: "여자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "지석환",
+    birthday: "980822",
+    gender: "남자",
+    job: "엔지니어",
+  },
+];
+
+class App extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>번호</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((customer) => {
+              return (
+                <Customer
+                  key={customer.id}
+                  id={customer.id}
+                  image={customer.image}
+                  name={customer.name}
+                  birthday={customer.birthday}
+                  gender={customer.gender}
+                  job={customer.job}
+                />
+              );
+            })}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
